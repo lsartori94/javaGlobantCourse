15e3fb3ce94509f88150d93fc8ad1210600118cc
Luca Sartori <lucasartori94@gmail.com>
2016-02-03 14:35:10 -0300

    Main method added

--- src/main/java/com/javaCourse/LucaSartori/controller/Main.java
+++ src/main/java/com/javaCourse/LucaSartori/controller/Main.java
@@ -3,8 +3,8 @@ package com.javaCourse.LucaSartori.controller;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Controller ctrl = new Controller();
+		ctrl.go();
 	}
 
 }
