5f1394d970d71e1112647a3bfa516eb2f1d46013
Luca Sartori <lucasartori94@gmail.com>
2016-02-04 14:04:43 -0300

    Views finished

--- src/main/java/com/javaCourse/LucaSartori/views/AdminMenu.java
+++ src/main/java/com/javaCourse/LucaSartori/views/AdminMenu.java
@@ -6,13 +6,14 @@ public class AdminMenu extends UserMenu {
 		super();
 		super.addItem("5. List Loans");
 		super.addItem("6. Approve Loan");
-		super.addItem("7. Add Comic");
-		super.addItem("8. Remove Comic");
-		super.addItem("9. Add Genre");
-		super.addItem("10. Remove Genre");
-		super.addItem("11. List Users");
-		super.addItem("12. Add User");
-		super.addItem("13. Remove User");
+		super.addItem("7. Reject Loan");
+		super.addItem("8. Add Comic");
+		super.addItem("9. Remove Comic");
+		super.addItem("10. Add Genre");
+		super.addItem("11. Remove Genre");
+		super.addItem("12. List Users");
+		super.addItem("13. Add User");
+		super.addItem("14. Remove User");
 		super.addItem("1000. Log Out");
 	}
 	
